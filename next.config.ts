cae5e6abb37f15f1355d63d33ed9c6970611e08e
naimekattor <naimekattor@gmail.com>
2026-05-16 11:44:03 +0600

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    images: {
-    domains: ['images.unsplash.com',"undeleterious-alecia-checked.ngrok-free.dev"], 
+    domains: ['images.unsplash.com',"undeleterious-alecia-checked.ngrok-free.dev","varivo.duckdns.org"], 
   },
 };
 
